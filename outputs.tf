da3128a93ba45ac4547c0b77cf5a1b8a3f1933fe
Yamil Solis <yamil@email.com>
2024-11-25 10:11:21 -0600

    Made some changes to outputs and added a data block in main

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "loadbalancer-dns" {
   description = "Accessing the Load Balancer"
   value = "Access load balancer with: http://${module.alb.alb-dns}"
 }
+
+output "instances-ip" {
+  description = "ASG Instances Public IP's"
+  value = data.aws_instances.asg_instances[*].public_ips
+}
