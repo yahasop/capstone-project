165b63affeeab9986af4d22e247d368185db22c2
Yamil Solis <yamil@email.com>
2024-12-17 18:36:41 -0600

    sh modified

--- user_data.sh
+++ user_data.sh
@@ -2,6 +2,7 @@
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt update -y
 sudo apt install jq sshpass openjdk-17-jdk ansible awscli -y
+sudo ansible-galaxy collection install community.docker
 sudo ufw allow 80
 sudo ufw allow 443
 echo "ubuntu:ubuntu" | sudo chpasswd
